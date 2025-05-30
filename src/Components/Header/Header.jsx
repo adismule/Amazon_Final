690777a41f405f3d4e9154da1cfed4ea1c241d8f
adismule <adismule@gmail.com>
2025-05-30 11:11:22 -0700

    final commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,3 @@
-// src/Components/Header/Header.jsx
 import React, { useContext } from 'react';
 import classes from './Header.module.css';
 import { SlLocationPin } from 'react-icons/sl';
@@ -9,10 +8,10 @@ import { DataContext } from '../DataProvider/DataProvider';
 
 
 const Header = () => {
-  const { basket, dispatch } = useContext(DataContext);
-  // const totalItem = basket.reduce((amount,item) => {
-  //   return item.amount + amount 
-  // },0)
+  const { state, dispatch } = useContext(DataContext);
+  const { basket } = state;
+  console.log(basket)
+
   const totalItem = basket?.reduce((amount, item) => item.amount + amount, 0) || 0;
   return (
     <>
