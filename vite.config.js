2299f3cda52124ef79ba58ef336ae892f83d3356
adismule <adismule@gmail.com>
2025-05-30 13:45:02 -0700

    modified configuration

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Amazon-Project-Class-2025/', // Ensure this matches your repo name
+});
