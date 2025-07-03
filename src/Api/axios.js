d87c2adfcaf570d3027eef8e45eb00c4aad9132e
adismule <adismule@gmail.com>
2025-07-03 08:57:09 -0700

    baseURL uncommented

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // deployed version of firebase functions 
-    // baseURL: "https://us-central1-clone-d72ca.cloudfunctions.net/api"
+    baseURL: "https://us-central1-clone-d72ca.cloudfunctions.net/api"
     // local instance of firebase functions
     // baseURL: "http://127.0.0.1:5001/clone-d72ca/us-central1/api"
 
